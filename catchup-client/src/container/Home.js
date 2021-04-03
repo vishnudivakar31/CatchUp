6eb31fd06b3c3b725c2a7e2c51fb03b2281f3d67
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-04-02 21:21:40 -0400

    ui initial design

--- catchup-client/src/container/Home.js
+++ catchup-client/src/container/Home.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import CallIcon from '@material-ui/icons/Call';
+import { nanoid } from 'nanoid'
+
+import "./Home.css"
 
 class Home extends Component {
     
     constructor(props) {
         super(props)
         this.socket = this.props.socket
+        this.uuid = nanoid(9)
     }
 
     componentDidMount() {
-        this.socket.emit("register", "aabcc-fffg")
+        this.socket.emit("register", this.uuid)
     }
 
     render() {
         return (
-            <div>
-                Home.....
+            <div className="home">
+                <div className="left-pane">
+                    <div className="brand">CatchUp</div>
+                    <div className="subtitle">
+                        CatchUp is an anonymous video chat solution for you to communicate with each other without worrying about companies collecting your data and spying on you.
+                    </div>
+                </div>
+                <div className="right-pane">
+                    <div className="id-title">Your ID:</div>
+                    <div className="your-id">{this.uuid}</div>
+                    <div className="input">
+                        <TextField 
+                            id="standard-basic"
+                            placeholder="enter your friend's ID to connect" 
+                            fullWidth
+                        />
+                        <Button
+                            variant='contained'
+                            color='primary'
+                            className='connect-button'
+                            startIcon={<CallIcon />}
+                        >
+                            Call
+                        </Button>
+                    </div>
+                </div>
             </div>
         )
     }
