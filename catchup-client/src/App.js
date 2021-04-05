9f3e67b79548ee60be6db06f7e95931b140af047
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-04-04 21:40:56 -0400

    ringing impl

--- catchup-client/src/App.js
+++ catchup-client/src/App.js
@@ -1,24 +1,56 @@
-import io from 'socket.io-client'
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom'
+import React, { Component } from 'react' 
+import Peer from 'peerjs'
 import Home from './container/Home'
+import { nanoid } from 'nanoid'
 
-const SERVER = "http://localhost:5000";
-let socket = io.connect(SERVER)
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.uuid = nanoid(9).replace(/[-_]/g, 'A')
+    this.peer = new Peer(this.uuid)
+    this.state = {
+      myConnection: undefined,
+      remoteConnection: undefined
+    }
+    this.setMyConnection = this.setMyConnection.bind(this)
+    this.setRemoteConnection = this.setRemoteConnection.bind(this)
+  }
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Home socket={socket} />
-        </Route>
-      </Switch>
-    </Router>
-  )
+  setMyConnection(conn) {
+    this.setState({
+      myConnection: conn
+    })
+  }
+  
+  setRemoteConnection(conn) {
+    this.setState({
+      remoteConnection: conn
+    })
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home 
+              peer={this.peer} 
+              uuid={this.uuid} 
+              myConnection={this.state.myConnection} 
+              remoteConnection={this.state.remoteConnection}
+              setMyConnection={this.setMyConnection}
+              setRemoteConnection={this.setRemoteConnection}
+            />
+          </Route>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
--- catchup-client/src/container/Home.js
+++ catchup-client/src/container/Home.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-import CallIcon from '@material-ui/icons/Call';
-import { nanoid } from 'nanoid'
+import CallIcon from '@material-ui/icons/Call'
+import Modal from '@material-ui/core/Modal'
+import Ringtone from '../sound/ringtone_minimal.wav'
+import Alert from '@material-ui/lab/Alert'
 
 import "./Home.css"
 
@@ -10,114 +12,145 @@ class Home extends Component {
     
     constructor(props) {
         super(props)
-        this.socket = this.props.socket
-        this.uuid = nanoid(9)
+        this.peer = this.props.peer
+        this.uuid = this.props.uuid
+        this.myConnection = this.props.myConnection
+        this.remoteConnection = this.props.remoteConnection
+        this.setRemoteConnection = this.props.setRemoteConnection
+        this.setMyConnection = this.props.setMyConnection
         this.callButtonTapped = this.callButtonTapped.bind(this)
-        this.initiateP2P = this.initiateP2P.bind(this)
-        this.setup = this.setup.bind(this)
-        this.onMessageForDC = this.onMessageForDC.bind(this)
-        this.createAnswer = this.createAnswer.bind(this)
-        this.invalidUUID = this.invalidUUID.bind(this)
-        this.onMessageForRemoteDC = this.onMessageForRemoteDC.bind(this)
-        this.setRemoteDescription = this.setRemoteDescription.bind(this)
-        this.dataChannelOpened = this.dataChannelOpened.bind(this)
-        this.setup()
+        this.showCallingModal = this.showCallingModal.bind(this)
+        this.showCallRejectedAlert = this.showCallRejectedAlert.bind(this)
+        this.showConnectingModal = this.showConnectingModal.bind(this)
+        this.state = {
+            calling: false,
+            calling_from: '',
+            call_rejected: false,
+            connecting: false
+        }
+        this.ringtone = new Audio(Ringtone)
+        this.ringtone.loop = true
     }
 
     componentDidMount() {
-        this.socket.emit("register", this.uuid)
-        this.socket.on('offer-recv', payload => this.createAnswer(payload))
-        this.socket.on('invalid-uuid', payload => this.invalidUUID(payload))
-        this.socket.on('answer-recv', payload => this.setRemoteDescription(payload))
+        this.peer.on('open', id => {
+            console.log('myID', id)
+        })
+        this.peer.on('error', e => console.log(e))
+        this.peer.on('connection', remoteConn => {
+            remoteConn.on('open', () => {
+                this.props.setRemoteConnection(remoteConn)
+                remoteConn.on('data', data => {
+                    if (data.message_type && data.message_type === 'calling') {
+                        this.setState({
+                            calling: true,
+                            calling_from: `${data.userID} is calling you.`
+                        })
+                    }
+                })
+            })
+        })
     }
 
-    setup() {
-        let config = {
-            iceServers: [
-                { 
-                    urls: 'stun:stun1.l.google.com:19302'
+    componentDidUpdate(_, prevState) {
+        if (this.state.calling !== prevState.calling) {
+            if (!this.state.calling) {
+                if(this.props.remoteConnection) {
+                    this.props.remoteConnection.send({
+                        message_type: 'call-declined'
+                    })
                 }
-            ]
+                this.ringtone.pause()
+            } else {
+                this.ringtone.play()
+            }
         }
-        this.localConnection = new RTCPeerConnection(config)
-        this.dataChannel = this.localConnection.createDataChannel('p2p-channel')
-        this.dataChannel.onmessage = this.onMessageForDC
-        this.dataChannel.onopen = this.dataChannelOpened
-
-        this.remoteConnection = new RTCPeerConnection()
     }
 
-    onMessageForDC(event) {
-        console.log('event data', event.data)
-    }
-
-    onMessageForRemoteDC(event) {
-        console.log('remote event', event.data)
-    }
-
-    dataChannelOpened(event) {
-        console.log('datachannel', this.dataChannel.readyState)
-        this.dataChannel.send("I hope you get this")
+    componentWillUnmount() {
+        this.ringtone.remove()
     }
 
     callButtonTapped() {
         if (this.friendsUUID && this.friendsUUID.value.length > 0) {
-            let friendsUUID = this.friendsUUID.value
-            this.initiateP2P(friendsUUID)
-        }
-    }
-
-    initiateP2P(uuid) {
-        console.log('offer created by', this.uuid, uuid)
-        this.localConnection.createOffer()
-        .then(offer => this.localConnection.setLocalDescription(offer))
-        .then(() => this.socket.emit('offer-send', {
-            targetUUID: uuid,
-            myUUID: this.uuid,
-            offer:this.localConnection.localDescription
-        }))
-    }
-
-    createAnswer(payload) {
-        let jsonPayload = JSON.parse(payload)
-        if (jsonPayload.targetUUID === this.uuid) {
-            console.log('answer created by', this.uuid, jsonPayload.myUUID)
-            this.remoteConnection.ondatachannel = e => {
-                this.remoteConnection.dc = e.channel
-                this.remoteConnection.dc.onmessage = this.onMessageForRemoteDC
-            }
-            this.remoteConnection.setRemoteDescription(jsonPayload.offer)
-            .then(() => {
-                this.remoteConnection.createAnswer()
-                .then(a => this.remoteConnection.setLocalDescription(a))
-                .then(() => {
-                    this.socket.emit('answer-send', {
-                        targetUUID: jsonPayload.myUUID,
-                        myUUID: this.uuid,
-                        answer: this.remoteConnection.localDescription
-                    })
+            let uuid = this.friendsUUID.value
+            let conn = this.peer.connect(uuid)
+            conn.on('open', () => {
+                this.setMyConnection(conn)
+                conn.send({
+                    message_type: 'calling',
+                    userID: this.uuid
                 })
+                conn.on('data', data => {
+                    if (data.message_type && data.message_type === 'call-declined') {
+                        this.setState({
+                            call_rejected: true,
+                            connecting: false
+                        })
+                    }
+                })
+                this.setState({ connecting: true })
+            })
+            conn.on('error', e => {
+                console.log(e)
             })
         }
     }
 
-    setRemoteDescription(payload) {
-        let jsonPayload = JSON.parse(payload)
-        if (jsonPayload.targetUUID === this.uuid) {
-            console.log('setting remote description by', this.uuid, jsonPayload.myUUID)
-            this.localConnection.setRemoteDescription(jsonPayload.answer)
-            .then(() => {
-                console.log('remote description set')
-                console.log('datachannel state', this.dataChannel.readyState)
-            })
-            .catch(e => console.log(e))
-        }
+    showCallingModal() {
+        return (
+            <Modal
+                open={this.state.calling}
+                disableBackdropClick={false}
+            >
+                <div className='calling_card'>
+                    <p>{this.state.calling_from}</p>
+                    <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10%' }}>
+                        <Button
+                            color='secondary'
+                            onClick={() => this.setState({
+                                calling: false,
+                                calling_from: ''
+                            })}
+                        >
+                            Reject
+                        </Button>
+                        <Button
+                            color='primary'
+                            variant='contained'
+                            style={{ marginLeft: '2%' }}
+                        >
+                            Accept
+                        </Button>
+                    </div>
+                </div>
+            </Modal>
+        )
     }
 
-    invalidUUID(payload) {
-        if (payload.myUUID === this.uuid) {
-            alert('invalid user')
-        }
+    showCallRejectedAlert() {
+        return (
+            <div style={{ display: this.state.call_rejected ? 'block' : 'none' }} className='alert-dialog'>
+                <Alert
+                    severity='warning'
+                    onClose={() => this.setState({ call_rejected: false })}
+                >
+                    Call rejected. User is busy. Try again later.
+                </Alert>
+            </div>
+        )
+    }
+
+    showConnectingModal() {
+        return (
+            <div style={{ display: this.state.connecting ? 'block' : 'none' }} className='alert-dialog'>
+                <Alert
+                    severity='info'
+                >
+                    Trying to connect with user. Please be patient.
+                </Alert>
+            </div>
+        )
     }
 
     render() {
@@ -150,6 +183,9 @@ class Home extends Component {
                         </Button>
                     </div>
                 </div>
+                {this.showCallingModal()}
+                {this.showCallRejectedAlert()}
+                {this.showConnectingModal()}
             </div>
         )
     }
